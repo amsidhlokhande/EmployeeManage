36b0bfaa1c268e7a2593aceb82e317c9eceeadf7
amsidhlokhande <amsidhlokhande@gmail.com>
2020-04-19 22:10:57 +0530

    Initial Commit

---
+++ java/com/amsidh/bean/Employee.java
@@ -0,0 +1,59 @@
+package com.amsidh.bean;
+
+public class Employee {
+	private Integer empId;
+	private String empName;
+	private String empCompany;
+	private Integer yearOfExperiance;
+	private String address;
+	private String emailId;
+
+	public Integer getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(Integer empId) {
+		this.empId = empId;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	public String getEmpCompany() {
+		return empCompany;
+	}
+
+	public void setEmpCompany(String empCompany) {
+		this.empCompany = empCompany;
+	}
+
+	public Integer getYearOfExperiance() {
+		return yearOfExperiance;
+	}
+
+	public void setYearOfExperiance(Integer yearOfExperiance) {
+		this.yearOfExperiance = yearOfExperiance;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+}
---
+++ java/com/amsidh/dom/EmployeeDom.java
@@ -0,0 +1,12 @@
+package com.amsidh.dom;
+
+public class EmployeeDom {
+
+	
+	private Integer id;
+	private String empName;
+	private String empCompany;
+	private Integer yearOfExperiance;
+	private String address;
+	private String emailId;
+}
